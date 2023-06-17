de1d432a504cf397abfe875b145b582683f1a371
YOUR NAME <YOUR EMAIL>
2023-06-17 15:01:08 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
+import Life from "./Life";
 class App extends React.Component {
-  state={
+  state = {
     fullname: "Adam neili",
     bio: "hellow",
-    image: "https://purepng.com/public/uploads/large/purepng.com-fortnite-ravenfortnitefortnite-battle-royalebattle-royaleepic-gamesgames-1251525436109rzdos.png",
+    image:
+      "https://purepng.com/public/uploads/large/purepng.com-fortnite-ravenfortnitefortnite-battle-royalebattle-royaleepic-gamesgames-1251525436109rzdos.png",
     profession: "student",
-    show: "false"
+    show: false,
+  };
+
+  render() {
+    const handelshow = () => {
+      this.setState({ show: !this.state.show });
+    };
+    return (
+      <div className="App">
+        <button onClick={handelshow}>
+          {this.state.show ? "hide" : "show"}
+        </button>
+        {this.state.show ? (
+          <div>
+            {" "}
+            <Life />
+            <h1>{this.state.fullname}</h1>
+            <h2>{this.state.bio}</h2>
+            <img src={this.state.image} alt="pic1" />
+            <h4>{this.state.profession}</h4>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    );
   }
-        
-  render(){
-    const handelshow=()=> {this.setState({show:!this.state.show})}
-  return (
-    <div className="App">
-    <button onClick={handelshow}>
-    {this.state.show? "hide" : "show"}
-    </button>
-    {this.state.show? 
-    <div><h1>{this.state.fullname}</h1>
-    <h2>{this.state.bio}</h2>
-    <img src={this.state.image} alt="pic1"/>
-    <h4>{this.state.profession}</h4>
-   </div>: "show"}
-    </div>
-  )
-}}
+}
 
 export default App;
